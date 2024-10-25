bf8dc9d5993ce169966ad24801e34d4e944698f8
Kevin García <kevingarciah58@gmail.com>
2024-10-24 21:54:17 -0600

    chore: scripts

--- CLASE11/script.bash
+++ CLASE11/script.bash
@@ -1,7 +1,7 @@
-mkdisk -size=5 -unit=K -fit=WF -path="/home/keviin/University/PRACTICAS/MIA_LAB_S2_2024/CLASE11/disks/DiscoLab.mia"
+mkdisk -size=5 -unit=K -fit=WF -path="/home/ubuntu/University/PRACTICAS/MIA_LAB_S2_2024/CLASE11/disks/DiscoLab.mia"
 
-fdisk -size=2 -type=P -unit=K -fit=BF -name="Particion1" -path="/home/keviin/University/PRACTICAS/MIA_LAB_S2_2024/CLASE11/disks/DiscoLab.mia"
+fdisk -size=2 -type=P -unit=K -fit=BF -name="Particion1" -path="/home/ubuntu/University/PRACTICAS/MIA_LAB_S2_2024/CLASE11/disks/DiscoLab.mia"
 
-mount -name="Particion1" -path="/home/keviin/University/PRACTICAS/MIA_LAB_S2_2024/CLASE11/disks/DiscoLab.mia"
+mount -name="Particion1" -path="/home/ubuntu/University/PRACTICAS/MIA_LAB_S2_2024/CLASE11/disks/DiscoLab.mia"
 
 mkfs -id=531A -fs=3fs
